2758a4cd718eb686cede8745df170a0d38288807
Prakash Kumar <Prakash@Prakashs-Air.lan>
2016-04-03 15:01:00 +0530

    added load staticfiles tag

--- django_template.sh
+++ django_template.sh
@@ -1,3 +1,4 @@
+
 li=$(grep -E '<link.*href' $1 | grep -v 'http\|{%\|{{\|.com' | grep -o "href.*" | cut -d'"' -f2)
 for i in ${li[@]}; do 
     sed -i -e "s#$i#{% static '$i' %}#g" $1; 
@@ -12,3 +13,4 @@ li=$(grep -E '<script.*src' $1 | grep -v 'http\|{%\|{{\|.com' | grep -o "src.*" 
 for i in ${li[@]}; do 
     sed -i -e "s#$i#{% static '$i' %}#g" $1; 
 done
+sed -i -e "1s#^#{% load staticfiles %}#" $1
